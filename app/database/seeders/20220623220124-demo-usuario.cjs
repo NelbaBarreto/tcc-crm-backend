b29ec82ace9152ccd6350ff322744a466e793f84
Nelba Barreto <barretonelba@gmail.com>
2022-09-18 15:28:13 -0400

    Agregar nuevo usuario en los seeds

--- app/database/seeders/20220623220124-demo-usuario.cjs
+++ app/database/seeders/20220623220124-demo-usuario.cjs
@@ -6,12 +6,18 @@ const up = async (queryInterface, _Sequelize) => {
     password: "123456",
     fec_insercion: new Date(),
     fec_modificacion: new Date(),
+  }, {
+    nom_usuario: "dtaboada",
+    email: "rkto737@gmail.com",
+    password: "123456",
+    fec_insercion: new Date(),
+    fec_modificacion: new Date(),
   }], {});
 };
 
 const down = async (queryInterface, _Sequelize) => {
   await queryInterface.bulkDelete("usuarios",
-      {email: "barretonelba@gmail.com"}, {});
+      {email: ["barretonelba@gmail.com", "rkto737@gmail.com"]}, {});
 };
 
 export {up, down};
