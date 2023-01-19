e7942145fb1a6f6548d9b5e13134d462037202a1
david-rkto <rkto737@gmail.com>
2023-01-19 14:32:15 -0300

    datos para dasboard

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -19,10 +19,77 @@ const casosPorEstado = async (req, res) => {
   } catch (error) {
     res.status(500).send({
       message:
-                error.message || "Ocurrió un error al intentar crear el caso.",
+                // eslint-disable-next-line max-len
+                error.message || "Ocurrió un error al intentar seleccionar el caso.",
     });
   }
 };
 
-export {casosPorEstado};
+// listar leads segun estado
+const leadsPorEstado = async (req, res) => {
+  try {
+    const data = await db.sequelize.query(
+        "SELECT count(1) as total, estado FROM leads GROUP BY ESTADO",
+        {
+          type: QueryTypes.SELECT,
+        },
+    );
+
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message:
+                // eslint-disable-next-line max-len
+                error.message || "Ocurrió un error al intentar seleccionar el lead.",
+    });
+  }
+};
+
+// listar llamadas segun estado
+const llamadasPorEstado = async (req, res) => {
+  try {
+    const data = await db.sequelize.query(
+        "SELECT count(1) as total, estado FROM llamadas GROUP BY ESTADO",
+        {
+          type: QueryTypes.SELECT,
+        },
+    );
+
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message:
+                // eslint-disable-next-line max-len
+                error.message || "Ocurrió un error al intentar seleccionar la llamada.",
+    });
+  }
+};
+
+// listar llamadas segun estado
+const casosPorPrioridad = async (req, res) => {
+  try {
+    const data = await db.sequelize.query(
+        "SELECT count(1) as total, PRIORIDAD FROM CASOS GROUP BY PRIORIDAD",
+        {
+          type: QueryTypes.SELECT,
+        },
+    );
+
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message:
+                // eslint-disable-next-line max-len
+                error.message || "Ocurrió un error al intentar seleccionar el caso segun prioridad.",
+    });
+  }
+};
+
+export {casosPorEstado, leadsPorEstado, llamadasPorEstado, casosPorPrioridad};
 
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,12 +1,15 @@
 /* eslint-disable new-cap */
 import {Router} from "express";
-import {casosPorEstado}
+import {casosPorEstado, leadsPorEstado, llamadasPorEstado, casosPorPrioridad}
   from "../controllers/dashboard.js";
 
 const RutasDashboard = (app) => {
   const router = Router();
 
   router.get("/casosPorEstado", casosPorEstado);
+  router.get("/leadsPorEstado", leadsPorEstado);
+  router.get("/llamadasPorEstado", llamadasPorEstado);
+  router.get("/casosPorPrioridad", casosPorPrioridad);
 
   app.use("/api/dashboard", router);
 };
