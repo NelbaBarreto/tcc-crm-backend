09ab555e8ab2efb6c5dc4ccc08bd928fbaafbb0b
Nelba Barreto <barretonelba@gmail.com>
2023-01-19 06:26:42 -0300

    Renombrar sucursales a sedes

---
+++ app/database/migrations/20220926230532-create-sedes.cjs
@@ -0,0 +1,48 @@
+/* eslint-disable new-cap */
+/* eslint-disable require-jsdoc */
+"use strict";
+
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable("sedes", {
+    sede_id: {
+      comment: "Identificador único de la sede.",
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    nombre: {
+      comment: "Nombre de la sede.",
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    // Auditoria
+    usu_insercion: {
+      allowNull: false,
+      type: Sequelize.STRING(20),
+      comment: "Nombre del usuario que insertó el registro.",
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      field: "fec_insercion",
+      comment: "Fecha en la que se creó el registro.",
+      defaultValue: Date.now(),
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+      field: "fec_modificacion",
+      comment: "Fecha en la que se modificó el registro por última vez.",
+      defaultValue: Date.now(),
+    },
+    usu_modificacion: {
+      allowNull: false,
+      type: Sequelize.STRING(20),
+      comment: "Nombre del usuario que modificó el registro por última vez.",
+    },
+  });
+}
+export async function down(queryInterface, _Sequelize) {
+  await queryInterface.dropTable("sedes");
+}
---
+++ app/routes/sedes.js
@@ -0,0 +1,19 @@
+/* eslint-disable new-cap */
+import {Router} from "express";
+import {create, findAll, findOne, update, _delete, deleteAll}
+  from "../controllers/sedes.js";
+
+const RutasSedes = (app) => {
+  const router = Router();
+
+  router.post("/", create);
+  router.get("/", findAll);
+  router.get("/:id", findOne);
+  router.put("/:id", update);
+  router.delete("/:id", _delete);
+  router.delete("/", deleteAll);
+
+  app.use("/api/sedes", router);
+};
+
+export default RutasSedes;
