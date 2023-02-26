fb26f17f39aae05403c86e97a8083503cefb6d32
Nelba Barreto <barretonelba@gmail.com>
2023-02-26 14:26:00 -0300

    Borrar foreign key de sedesd

--- app/database/migrations/20230224073906-delete-models-2.cjs
+++ app/database/migrations/20230224073906-delete-models-2.cjs
@@ -4,6 +4,8 @@
 
 export async function up(queryInterface, _Sequelize) {
   await queryInterface.removeColumn("telefonos", "sede_id");
+  await queryInterface.removeColumn("direcciones", "sede_id");
+  await queryInterface.removeColumn("curso_ciclos", "sede_id");
   await queryInterface.dropTable("sedes");
 }
 export async function down(_queryInterface, _Sequelize) {
